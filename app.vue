56a0111a3934436f391048f86d44f7ffe9142a15
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-08-10 00:27:59 +0900

    fix: Set the style for SignaturePad.

--- app.vue
+++ app.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import SignaturePad from "signature_pad";
 
-const canvas = ref();
+const canvas = ref<HTMLCanvasElement>({} as HTMLCanvasElement);
 
 onMounted(async () => {
-  new SignaturePad(canvas.value);
+  canvas.value.width = 600;
+  canvas.value.height = 300;
+  new SignaturePad(canvas.value, {
+    backgroundColor: "rgba(255,255,255)",
+  });
 });
 </script>
 
 <template>
-  <div>
-    <canvas ref="canvas"></canvas>
+  <div class="bg-slate-100 h-svh flex">
+    <canvas class="m-auto" ref="canvas"></canvas>
   </div>
 </template>
