1e0c3a6f7ff844e5760f4452c46679360dcae410
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-08-09 23:49:57 +0900

    Introducing signature_pad

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+import SignaturePad from "signature_pad";
+
+const canvas = ref();
+
+onMounted(async () => {
+  new SignaturePad(canvas.value);
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <canvas ref="canvas"></canvas>
   </div>
 </template>
